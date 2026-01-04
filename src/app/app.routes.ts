54435e24847e4b55218eef13071cfa5904e6c6e7
huy-gv <emperorhuyv@gmail.com>
2026-01-04 11:58:18 +1100

    defined major pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { GameplayPage } from './pages/gameplay-page/gameplay-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: HomePage,
+        path: "home"
+    },
+    {
+        component: GameplayPage,
+        path: "gameplay"
+    }
+];
---
+++ src/app/pages/gameplay-page/gameplay-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gameplay-page',
+  imports: [],
+  templateUrl: './gameplay-page.html',
+  styleUrl: './gameplay-page.css',
+})
+export class GameplayPage {
+
+}
