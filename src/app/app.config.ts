dda3a1c60bdc644d3ef37252f434d2fa25ea344c
huy-gv <emperorhuyv@gmail.com>
2026-01-07 20:04:30 +1100

    added firestore configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,24 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "osint-challenge-10e24",
+      appId: "1:487086894126:web:ecce9f99da71f761ce03bf",
+      storageBucket: "osint-challenge-10e24.firebasestorage.app",
+      apiKey: "AIzaSyDViEzt5o4VyXcGezApwHcvqlrEDuOqqsk",
+      authDomain: "osint-challenge-10e24.firebaseapp.com",
+      messagingSenderId: "487086894126",
+      // projectNumber: "487086894126",
+      // version: "2"
+    })),
+    provideFirestore(() => getFirestore())
   ]
 };
